eb384111eb5577b0086f47a7044d02b68c73c321
Mike Barberry <mikebarberry@protonmail.com>
2020-09-30 02:59:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.length == 1
     return array.join
   elsif array.length > 3
-    return [0..-2]array.join(", ") + " and " + array[-1]
+    return array[0..-2].join(", ") + " and " + array[-1]
   end
 end
 
